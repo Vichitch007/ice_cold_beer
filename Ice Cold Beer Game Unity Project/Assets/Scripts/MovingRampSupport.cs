adc5b0f0ac005eaebea491bf2e3dc6c9b11f4efb
Victor <55261794+Vichitch007@users.noreply.github.com>
2019-10-10 18:06:20 -0500

    Changed move system to use physics

--- Ice Cold Beer Game Unity Project/Assets/Scripts/MovingRampSupport.cs
+++ Ice Cold Beer Game Unity Project/Assets/Scripts/MovingRampSupport.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class MovingRampSupport : MonoBehaviour
 {
-    private float moveSpeed = 0.1f;
+    public float moveSpeed = 0.1f;
+
+    private float verticalInput;
+    private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World.");
-        //transform.Translate(5, 0, 0);
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(1, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Use fixed update for physics code, because we need to be
+    // Careful about how often we call expensive, hardware intensive,
+    // physics stuff.
+    private void FixedUpdate()
     {
-        // if the player presses up arrow, we move the square up.
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(0, moveSpeed, 0);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(0, -moveSpeed, 0);
-        }
+        verticalInput = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector2(0, verticalInput * moveSpeed);
     }
 }
