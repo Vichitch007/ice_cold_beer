de256698a640b39ed7c5a9f5c701c1a564842634
Victor <55261794+Vichitch007@users.noreply.github.com>
2019-11-14 17:09:09 -0600

    Added Obstacle functionality

---
+++ Ice Cold Beer Game Unity Project/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public GameObject gameOverText;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touched Trigger");
+        if (collision.tag == "Player")
+        {
+            gameOverText.SetActive(true);
+            audioSource.Play();
+            Debug.Log("Player has touched the Obstacle!");
+        }
+    }
+}
